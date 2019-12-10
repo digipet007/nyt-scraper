d1ab1a80063c83a4e9be26711eff1815c0a4540e
Sarah Arnold <digipet007@gmail.com>
2019-12-10 08:14:22 -0800

    cleaned up code

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -5,27 +5,15 @@ const axios = require("axios");
 const scrape = function(cb) {
   axios.get("https://www.nytimes.com").then(function(response) {
     // Load the HTML into cheerio and save it to a variable
-    // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+    // '$' becomes a shorthand for cheerio's selector commands
     var $ = cheerio.load(response.data);
 
     // An empty array to save the data that we'll scrape
     var results = [];
-    // var results = {};
 
     // Select each element in the HTML body from which you want information.
     // NOTE: Cheerio selectors function similarly to jQuery's selectors
-    // $(".theme-summary").each(function(i, element) {
-    // $("div.esl82me1").each(function(i, element) {
     $("article.css-8atqhb").each(function(i, element) {
-      // var head = $(element)
-      //   .children(".esl82me0")
-      //   .text()
-      //   .trim();
-      // var sum = $(element)
-      //   .children(".e1n8kpyg0")
-      //   .text()
-      //   .trim();
-
       headline = $(element)
         .find("h2")
         .text()
@@ -48,24 +36,15 @@ const scrape = function(cb) {
         url += "URL Unavailable";
       }
 
-      // if (head && sum) {
-      //   var headNeat = head.replace(/(\r\n|\n|r|\t|\s+)/gm, " ").trim();
-      //   var sumNeat = sum.replace(/(\r\n|\n|r|\t|\s+)/gm, " ").trim();
-
       var dataToAdd = {
         headline: headline,
         summary: summary,
         url: url
-        // headline: headNeat,
-        // summary: sumNeat
       };
 
       results.push(dataToAdd);
-      // }
     });
     cb(results);
-    // Log the results once you've looped through each of the elements found with cheerio
-    console.log(results);
   });
 };
 
