954ceb049d6d3b64b5076edc47bb877fb6c3a19d
Sarah Arnold <digipet007@gmail.com>
2019-12-10 21:51:03 -0800

    cleaned up code

--- config/routes.js
+++ config/routes.js
@@ -35,9 +35,6 @@ module.exports = function(router) {
 
   //api route deletes headline with specific id
   router.delete("/api/headlines/delete/:id", function(req, res) {
-    // console.log("======================================");
-    // console.log("req.body._id from routes.js router.put api/headlines/delete/:id");
-    // console.log(req.body._id);
     headlinesController.delete(req.body._id, function(err, data) {
       res.json(data);
     });
@@ -46,14 +43,7 @@ module.exports = function(router) {
   //Route updates headlines, as needed. Runs headlines controller update function on whatever the user sends in their request
   //NOTE: patch??
   router.put("/api/headlines/:id", function(req, res) {
-    console.log("======================================");
-    console.log("req.body._id from routes.js api/headlines/:id");
-    console.log(req.body._id);
     headlinesController.update(req.body._id, function(err, data) {
-      ///problem: cannot return data from doc
-      // console.log("======================================");
-      // console.log("data from routes.js api/headlines:");
-      // console.log(data); //data is undefined
       res.json(data);
     });
   });
@@ -61,30 +51,13 @@ module.exports = function(router) {
   //Route grabs all notes associated with an article to display to user
   //Runs route on notes associated with headline id
   router.get("/api/notes/:id", function(req, res) {
-    // var queryId = {};
-    console.log("=============changed============="); //========need to return ehre note id matches
-    console.log("router.get req.params");
-    console.log(req.params);
-    // if (req.params.headline) {
-    // queryId = req.params.headline;
-    // }
-
     notesController.get(req.params, function(data) {
       res.json(data);
-      console.log("==========================");
-      console.log("router.get returned data");
-      console.log(data);
-      // console.log(err);
     });
   });
 
   //Route for deleting notes
   router.delete("/api/notes/delete/", function(req, res) {
-    // var queryId = {};
-    // queryId = req.params.id;
-    console.log("==================");
-    console.log("router req.body");
-    console.log(req.body);
     notesController.delete(req.body, function(err, data) {
       res.json(data);
     });
@@ -92,9 +65,6 @@ module.exports = function(router) {
 
   //route for posting new notes to articles
   router.post("/api/notes", function(req, res) {
-    console.log("===============================");
-    console.log("routes.js post req.body :");
-    console.log(req.body);
     notesController.save(req.body, function(data) {
       res.json(data);
     });
